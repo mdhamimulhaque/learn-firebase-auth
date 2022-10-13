41e9229b30cc486779386ceaae1c0572770aa5d7
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-13 08:45:29 +0600

    google sign in method

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBO-Vip02NdjjuC7gGglcNOqhEpKfhJ1bk",
+    authDomain: "learn-firebase-auth-7e532.firebaseapp.com",
+    projectId: "learn-firebase-auth-7e532",
+    storageBucket: "learn-firebase-auth-7e532.appspot.com",
+    messagingSenderId: "311238420540",
+    appId: "1:311238420540:web:9e2efaa11c95ed8c187b39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
