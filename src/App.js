1a06d383d72bcbd91da5ec00363f406abb2f0818
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-10-13 09:26:02 +0600

    sign out  system with condition

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import './App.css';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase/firebase.init';
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(res => {
         const user = res.user;
-        console.log(user)
+        setUser(user)
       }).catch(err => {
+        console.error(err)
+      })
+  }
+
+  // ---> sign out
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(err => {
         console.error(err)
       })
   }
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <div className="google_container">
+        <h2>Google Auth</h2>
 
+        {
+          user.email &&
+          <div className="info_wrapper">
+            <img src={user?.photoURL} alt="" />
+            <h2>Welcome {user?.displayName}</h2>
+            <p>Email : {user?.email}</p>
+          </div>
+        }
+
+        {
+          user.email ?
+            <button className='g_signOut_btn' onClick={handleSignOut}>Sign Out</button>
+            :
+            <button className='g_signIn_btn' onClick={handleGoogleSignIn}>Google Sign In</button>
+        }
+
+      </div>
     </div>
   );
 }
